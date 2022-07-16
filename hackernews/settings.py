032b65eb4fdc6e12f4da9c18714ff60f610cdc04
dtbhushan <dtbhushan@gmail.com>
2022-07-16 17:40:55 +0530

    changed database setting to default back

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -83,28 +83,25 @@ WSGI_APPLICATION = 'hackernews.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-import dj_database_url
 
-DATABASES = {'default': dj_database_url.config(default='postgres://eoaoxdobbmftih:d83a269f7f3f67c26964b150afac78bef36819f579c2ce8bf18526a3bcfbbeed@ec2-54-152-28-9.compute-1.amazonaws.com:5432/ddfit97br7offt:bar@localhost:5432/postgres')}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD' : 'aditya1',
-#         'HOST' : 'localhost',
-#         'PORT' : '5432',
-#     }
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.postgresql',
-#     #     'NAME': 'ddfit97br7offt',
-#     #     'USER': 'eoaoxdobbmftih',
-#     #     'PASSWORD' : 'd83a269f7f3f67c26964b150afac78bef36819f579c2ce8bf18526a3bcfbbeed',
-#     #     'HOST' : 'ec2-54-152-28-9.compute-1.amazonaws.com',
-#     #     'PORT' : '5432',
-#     # }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'PASSWORD' : 'aditya1',
+    #     'HOST' : 'localhost',
+    #     'PORT' : '5432',
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ddfit97br7offt',
+        'USER': 'eoaoxdobbmftih',
+        'PASSWORD' : 'd83a269f7f3f67c26964b150afac78bef36819f579c2ce8bf18526a3bcfbbeed',
+        'HOST' : 'ec2-54-152-28-9.compute-1.amazonaws.com',
+        'PORT' : '5432',
+    }
+}
 
 
 
